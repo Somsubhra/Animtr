3c5da9c112b75730b0826a5be440842e56142afb
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-11-18 20:51:15 +0530

    Add layer and frame box scroll areas to timeline

--- views/dockers/timeline/timeline.cpp
+++ views/dockers/timeline/timeline.cpp
@@ -1,10 +1,32 @@
 // Other includes
 #include "timeline.h"
 
+// Qt includes
+#include <QGridLayout>
+#include <QScrollArea>
+#include <QSplitter>
+
 Timeline::Timeline(QWidget *parent) :
     Docker(parent)
 {
     setWindowTitle(tr("Timeline"));
+
+    QGridLayout* mainLayout = new QGridLayout();
+    QWidget* mainWidget = new QWidget(this);
+
+    QScrollArea* layerBoxScrollArea = new QScrollArea(mainWidget);
+    QScrollArea* frameBoxScrollArea = new QScrollArea(mainWidget);
+
+    QSplitter* splitter = new QSplitter(mainWidget);
+    splitter->addWidget(layerBoxScrollArea);
+    splitter->addWidget(frameBoxScrollArea);
+
+    mainLayout->addWidget(splitter, 0, 0);
+
+    mainWidget->setLayout(mainLayout);
+    setWidget(mainWidget);
+
+    setMaximumHeight(200);
 }
 
 Qt::DockWidgetArea Timeline::defaultDockerArea()
